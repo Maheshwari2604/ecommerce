b3fac2942f5d075b439a24573d3d0245b704d5cd
Maheshwari2604 <maheshwarishivam2604@gmail.com>
2019-01-26 13:47:17 +0530

    ecommerce_project

---
+++ ecommerce/ecommerce/urls.py
@@ -0,0 +1,38 @@
+"""ecommerce URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+from user_model.views import user_signup, user_login, activate, user_home,password_reset, activate_password, password_reset_new
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^register_user/', user_signup, name='user_signup'),
+    url(r'^login_user/', user_login, name='user_login'),
+    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        activate, name='activate'),
+    url(r'^activate_password/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        activate_password, name='activate_password'),
+    url(r'^$', user_home , name='user_home'),
+    url(r'^password_reset/', password_reset , name='password_reset'),
+    url(r'^password_reset_new/', password_reset_new, name='password_reset_new'),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
